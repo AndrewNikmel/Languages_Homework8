659352b67bb74b4739a6f4a9440af325de1af5bb
unknown <Andrew M>
2022-12-26 22:45:36 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿
+
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -7,6 +10,55 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateRandom2dArray(int rows, int cols, int min, int max){
+    int[,] array = new int[rows, cols];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            array[i,j]=new Random().Next(min, max+1);
+            
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortRows(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1)-i-1; j++){
+            if(array[i,j]>array[i,j+1]){
+                int max = array[i,j];
+                array[i,j] = array[i,j+1];
+                array[i,j+1] = max;
+                Console.Write(max + " ");
+            }
+        }
+        
+    }
+
+}
+
+
+Console.WriteLine("Enter the amount of the rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the amount of the columns: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the amount of the minimal: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the amount of the maximal: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreateRandom2dArray(rows,cols,min,max);
+Show2dArray(array);
+SortRows(array);
+
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
